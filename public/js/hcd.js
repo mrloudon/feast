da7e0c5e19704a6b9de72d192feff14a4c0c4018
Malcolm Loudon <m.r.loudon@gmail.com>
2022-06-09 15:53:08 +1200

    added task 2

--- public/js/hcd.js
+++ public/js/hcd.js
@@ -1,7 +1,9 @@
 import * as Utility from "./utility.js";
 
-const tasks = [doWelcomePage, doPracticeInstructions1Page, doPracticeInstructions2Page,
+const tasks = [doPracticeTask2, doPracticeTask2Instructions1Page, doPracticeTask2Instructions2Page, doWelcomePage, doPracticeTask1Instructions1Page, doPracticeTask1Instructions2Page,
     doReactionTimePage];
+
+const symbolImages = ["img/dislike-extremely.png", "img/dislike-moderately.png", "img/dislike-slightly.png"];
 
 const bodyKeys = {
     keyX: false,
@@ -35,8 +37,8 @@ function doWelcomePage(callback) {
     Utility.fadeIn(page);
 }
 
-function doPracticeInstructions1Page(callback) {
-    const page = document.getElementById("practice-instructions-1-page");
+function doPracticeTask1Instructions1Page(callback) {
+    const page = document.getElementById("practice-task-1-instructions-1-page");
     const nextBtn = page.querySelector("button.next-btn");
 
     function nextBtnClick() {
@@ -51,8 +53,43 @@ function doPracticeInstructions1Page(callback) {
     Utility.fadeIn(page);
 }
 
-function doPracticeInstructions2Page(callback) {
-    const page = document.getElementById("practice-instructions-2-page");
+function doPracticeTask1Instructions2Page(callback) {
+    const page = document.getElementById("practice-task-1-instructions-2-page");
+
+    function keyDown(evt) {
+        if (evt.keyCode === 32) {
+            document.body.removeEventListener("keydown", keyDown);
+            evt.preventDefault();
+            evt.stopPropagation();
+            Utility.fadeOut(page)
+                .then(callback);
+            return false;
+        }
+    }
+
+    hideJumbos();
+    document.body.addEventListener("keydown", keyDown);
+    Utility.fadeIn(page);
+}
+
+function doPracticeTask2Instructions1Page(callback) {
+    const page = document.getElementById("practice-task-2-instructions-1-page");
+    const nextBtn = page.querySelector("button.next-btn");
+
+    function nextBtnClick() {
+        nextBtn.removeEventListener("click", nextBtnClick);
+        Utility.fadeOut(page)
+            .then(() => {
+                callback();
+            });
+    }
+
+    nextBtn.addEventListener("click", nextBtnClick);
+    Utility.fadeIn(page);
+}
+
+function doPracticeTask2Instructions2Page(callback) {
+    const page = document.getElementById("practice-task-2-instructions-2-page");
 
     function keyDown(evt) {
         if (evt.keyCode === 32) {
@@ -137,6 +174,89 @@ function doReactionTimePage(callback) {
     warningDiv.classList.add("invisible");
     document.body.addEventListener("keydown", keyDown);
 
+    Utility.fadeIn(page)
+        .then(showStimulus);
+}
+
+function doPracticeTask2(callback){
+    const page = document.getElementById("practice-task-2-stimulus-page");
+    const warningDiv = page.querySelector(".warning-div");
+    const img = page.querySelector("img");
+    const wordDiv = page.querySelector(".word-div");
+
+    const words = ["Red", "Dog", "Rock"];
+
+    const ITI = 1000;
+    const N_TRIALS = 3;
+    const MAX_RT = 500;
+    const WARNING_TIME = 250;
+
+    let acceptKeypresses = false;
+    let currentTrial = 0;
+    let tooSlowTimer;
+    let tooSlow = false;
+    let rts = [], startTime;
+
+    function showStimulus() {
+        setTimeout(() => {
+            console.log(currentTrial);
+            if(currentTrial === N_TRIALS){
+                console.log(rts);
+                console.log(rts.length);
+                Utility.fadeOut(page)
+                    .then(() => {
+                        showJumbos();
+                        callback();
+                    });
+                return;
+            }
+            wordDiv.innerHTML = words[currentTrial];
+            currentTrial++;
+            startTime = Date.now();
+            acceptKeypresses = true;
+            tooSlow = false;
+            tooSlowTimer = setTimeout(() => {
+                console.log("too slow");
+                wordDiv.innerHTML = "&nbsp;";
+                warningDiv.classList.remove("invisible");
+                acceptKeypresses = false;
+                setTimeout(() => {
+                    acceptKeypresses = true;
+                }, WARNING_TIME);
+                tooSlow = true;
+                if(currentTrial > 0){
+                    currentTrial--;
+                }
+            }, MAX_RT);
+        }, ITI);
+
+    }
+
+    function keyDown(evt) {
+        if (acceptKeypresses && evt.keyCode === 32) {
+            acceptKeypresses = false;
+            wordDiv.innerHTML = "&nbsp;";
+            if(tooSlow){
+                warningDiv.classList.add("invisible");
+            }
+            else{
+                rts.push(Date.now() - startTime);
+                window.clearTimeout(tooSlowTimer);
+            }
+            evt.preventDefault();
+            evt.stopPropagation();
+            showStimulus();
+            return false;
+        }
+    }
+
+    acceptKeypresses = false;
+    currentTrial = 0;
+    hideJumbos();
+    warningDiv.classList.add("invisible");
+    wordDiv.innerHTML = "&nbsp;";
+    img.src = symbolImages[0];
+    document.body.addEventListener("keydown", keyDown);
     Utility.fadeIn(page)
         .then(showStimulus);
 }
